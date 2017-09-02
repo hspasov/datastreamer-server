bf8eaf21f95290d05299f72cbb19e4894e9b4e47
Hristo Spasov <hristo_sp99@abv.bg>
2017-09-02 15:12:43 +0300

    Add sample sending from server to client

--- sockets/base.js
+++ sockets/base.js
@@ -1,12 +1,18 @@
 module.exports = function (io) {
     // io stuff here... io.on('conection.....
     io.on('connection', function (socket) {
-        console.log('a user connected');
+        console.log(`${socket.id} connected`);
         socket.on('demo', function (msg) {
             console.log('message: ' + msg);
         });
         socket.on('disconnect', function () {
-            console.log('user disconnected');
+            console.log(`${socket.id} disconnected`);
+        });
+        socket.on('serverHandshake', function (msg) {
+            console.log(msg);
+            // socket.to(socket.id).emit('serverHandshake', `Hello, ${socket.id}, from localhost!`);
+            io.to(socket.id).emit('serverHandshake', `Hello, ${socket.id}, from localhost!`);
+            console.log("broadcasted!");
         });
     });
 }
