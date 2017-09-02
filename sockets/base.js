44276b75415f29d9f4eb4e39525aa204c86112eb
Hristo Spasov <hristo_sp99@abv.bg>
2017-09-02 18:01:30 +0300

    Implement getting file metadata from provider

--- sockets/base.js
+++ sockets/base.js
@@ -1,16 +1,16 @@
 module.exports = function (io) {
-    // io stuff here... io.on('conection.....
     io.on('connection', function (socket) {
         console.log(`${socket.id} connected`);
-        socket.on('demo', function (msg) {
+        socket.on('demo', function (msg, metadata) {
             console.log('message: ' + msg);
+            console.log('metadata:');
+            console.log(metadata);
         });
         socket.on('disconnect', function () {
             console.log(`${socket.id} disconnected`);
         });
         socket.on('serverHandshake', function (msg) {
             console.log(msg);
-            // socket.to(socket.id).emit('serverHandshake', `Hello, ${socket.id}, from localhost!`);
             io.to(socket.id).emit('serverHandshake', `Hello, ${socket.id}, from localhost!`);
             console.log("broadcasted!");
         });
