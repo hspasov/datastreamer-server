c250e959ae5d99cbde4e8bf5e54ff9d8412f8583
Hristo Spasov <hristo_sp99@abv.bg>
2017-11-21 22:48:38 +0200

    Refactoring

--- sockets/client.js
+++ sockets/client.js
@@ -1,79 +1,77 @@
 import io from "socket.io-client";
 
-class Socket {
-    constructor(RTC, token) {
-        this.RTC = RTC;
-        this.socket = io(`https://${window.location.host}`, {
-            query: `token=${token}`,
-            secure: true
-        });
+function Socket(RTC, token) {
+    this.RTC = RTC;
+    this.socket = io(`https://${window.location.host}`, {
+        query: `token=${token}`,
+        secure: true
+    });
 
-        this.socket.on("connectToProviderSuccess", () => {
-            console.log("Successfully connected");
-        });
+    this.socket.on("connectToProviderSuccess", () => {
+        console.log("Successfully connected");
+    });
 
-        this.socket.on("connectToProviderFail", error => {
-            switch (error) {
-                case "TokenExpiredError":
-                    console.log("Token Expired");
-                    break;
-                case "JsonWebTokenError":
-                    console.log("JsonWebTokenError");
-                    break;
-                case "ProviderNotConnectedError":
-                    console.log("connect to provider failed");
-                    break;
-                default:
-                    console.log("Something went wrong...");
+    this.socket.on("connectToProviderFail", error => {
+        switch (error) {
+            case "TokenExpiredError":
+                console.log("Token Expired");
+                break;
+            case "JsonWebTokenError":
+                console.log("JsonWebTokenError");
+                break;
+            case "ProviderNotConnectedError":
+                console.log("connect to provider failed");
+                break;
+            default:
+                console.log("Something went wrong...");
+        }
+        this.RTC.deleteP2PConnection();
+    });
+
+    this.socket.on("resetConnection", () => {
+        this.RTC.deleteP2PConnection();
+        this.RTC.initializeP2PConnection();
+    });
+
+    this.socket.on("requestedP2PConnection", () => {
+        this.RTC.initializeP2PConnection();
+    });
+
+    this.socket.on("receiveProviderDescription", description => {
+        try {
+            console.log("inside receiveProviderDescription:")
+            console.log(description);
+            this.RTC.peerConnection.setRemoteDescription(JSON.parse(description));
+        } catch (e) {
+            if (!this.RTC.peerConnection) {
+                console.log("Connection to provider lost.");
+            } else {
+                throw e;
             }
-            this.RTC.deleteP2PConnection();
-        });
+        }
+    });
 
-        this.socket.on("resetConnection", () => {
-            this.RTC.deleteP2PConnection();
-            this.RTC.initializeP2PConnection();
-        });
-
-        this.socket.on("requestedP2PConnection", () => {
-            this.RTC.initializeP2PConnection();
-        });
-
-        this.socket.on("receiveProviderDescription", description => {
-            try {
-                console.log("inside receiveProviderDescription:")
-                console.log(description);
-                this.RTC.peerConnection.setRemoteDescription(JSON.parse(description));
-            } catch (e) {
-                if (!this.RTC.peerConnection) {
-                    console.log("Connection to provider lost.");
-                } else {
-                    throw e;
+    this.socket.on("receiveICECandidate", candidate => {
+        try {
+            console.log("inside receiveICECandidate");
+            console.log(candidate);
+            this.RTC.peerConnection.addIceCandidate(JSON.parse(candidate)).then(
+                () => { },
+                error => {
+                    console.log("failed to add candidate", error);
+                    this.RTC.deleteP2PConnection(error);
                 }
+            );
+        } catch (e) {
+            if (!this.RTC.peerConnection) {
+                console.log("Connection to provider lost.");
+            } else {
+                throw e;
             }
-        });
+        }
+    });
 
-        this.socket.on("receiveICECandidate", candidate => {
-            try {
-                console.log("inside receiveICECandidate");
-                console.log(candidate);
-                this.RTC.peerConnection.addIceCandidate(JSON.parse(candidate)).then(
-                    () => { },
-                    error => {
-                        console.log("failed to add candidate", error);
-                        this.RTC.deleteP2PConnection(error);
-                    }
-                );
-            } catch (e) {
-                if (!this.RTC.peerConnection) {
-                    console.log("Connection to provider lost.");
-                } else {
-                    throw e;
-                }
-            }
-        });
-
-        console.log("connecting to provider");
-    }
+    console.log("connecting to provider");
 }
 
 export default Socket;
